42527e56c011b0f1c782f932548050c76a2aace7
linfu123 <1025595122@qq.com>
2020-02-03 20:29:27 +0800

    测试提交

--- src/main/java/com/company/project/weixin/MenuKey.java
+++ src/main/java/com/company/project/weixin/MenuKey.java
@@ -20,7 +20,7 @@ public class MenuKey {
     //我的共享记录
     public static final String SHARE_RECORD = "btn_share_record";
 
-    public static final String URL = "http://6kivds.natappfree.cc/pybl/";
+    public static final String URL = "http://pue5z5.natappfree.cc/pybl/";
     //朋悦比邻
 //    public static final String URL = "http://f.pyblkj.cn/pybl/";
 

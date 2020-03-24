3e875c79d4034ab75a1cd7b96f9a456e5476afdd
jifei <jifeiyu338@163.com>
2020-03-24 12:50:06 +0800

    修改界面，查看返回值

--- src/main/resources/static/js/myUtil.js
+++ src/main/resources/static/js/myUtil.js
@@ -66,7 +66,14 @@ if (getCookie('openId')===""){
 return false;
 }
 
-
+window.alert = function(name){
+    var iframe = document.createElement("IFRAME");
+    iframe.style.display="none";
+    iframe.setAttribute("src", 'data:text/plain,');
+    document.documentElement.appendChild(iframe);
+    window.frames[0].window.alert(name);
+    iframe.parentNode.removeChild(iframe);
+}
 function isEmpty(v) {
     switch (typeof v) {
         case 'undefined':

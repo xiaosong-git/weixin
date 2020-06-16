e30f620c5dee82640ccbcbbf6fe6db2754a7909f
jifei <jifeiyu338@163.com>
2020-06-16 15:12:20 +0800

    修改addr

--- src/main/java/com/company/project/weixin/Menu.java
+++ src/main/java/com/company/project/weixin/Menu.java
@@ -60,7 +60,7 @@ public class Menu {
 //        String url=null;
 //        try {
 //            //通过公众号访问地址授权
-//            url = iService.oauth2buildAuthorizationUrl("http://x946wf.natappfree.cc/school/#/BindPhone", "snsapi_userinfo", "233");
+//            url = iService.oauth2buildAuthorizationUrl("http://b99dib.natappfree.cc/school/#/BindPhone", "snsapi_userinfo", "233");
 //        }catch (WxErrorException w){
 //            w.getStackTrace();
 //        }
